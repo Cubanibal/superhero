4ecee704008add0080cfe9f33d6d7bc56c68f0a1
Anibal Montero <cubanibal@hotmail.com>
2021-11-17 17:05:18 +0100

    team terminado

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -36,5 +36,6 @@ class HomeController < ApplicationController
   end
 
   def team
+    @team_number = params[:team_number].to_i
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
   get 'home/team'
+  post 'home/team' => 'home#team'
   
 end
